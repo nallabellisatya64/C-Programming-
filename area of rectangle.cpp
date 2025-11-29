aea4201d380fbfc6957048a920de31154bd50c8a
nallabellisatya64 <nallabellisatya64@gmail.com>
2025-11-29 09:43:34 +0530

    Add files via upload

---
+++ area of rectangle.cpp
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+	float l,b,area;
+	printf("enter l value");
+	scanf("%f",&l);
+	printf("enter b value");
+	scanf("%f",&b);
+	area=l*b;
+	printf("area of rectangle=%f",area);
+	return 0;
+}
