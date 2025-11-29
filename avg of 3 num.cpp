9f8c1166b32a3044e1f10f52aa12b0429022bb63
nallabellisatya64 <nallabellisatya64@gmail.com>
2025-11-29 09:45:23 +0530

    Add files via upload

---
+++ avg of 3 num.cpp
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+	int a,b,c,d;
+	printf("enter a value::");
+	scanf("%d",&a);
+	printf("\n enter b value::");
+	scanf("%d",&b);
+	printf("enter c value::");
+	scanf("%d",&c);
+	d=(a+b+c)/3;
+	printf("average of three numbers=%d",d);
+	return 0;
+}
