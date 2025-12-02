396c017c6061fa3590c78286ee110f19578e97d8
nallabellisatya64 <nallabellisatya64@gmail.com>
2025-12-02 16:15:03 +0530

    Add files via upload

---
+++ sum of array elements.cpp
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+	int a[100],n,i,sum=0;
+	printf("Enter size of the Array");
+	scanf("%d",&n);
+	printf("Enter array value");
+	for(i=0;i<n;i++)
+	{
+		scanf("%d",&a[i]);
+	}
+	for(i=0;i<5;i++)
+	{
+		sum=sum+a[i];
+	}
+	printf("sum of array values=%d",sum);
+	return 0;
+}
