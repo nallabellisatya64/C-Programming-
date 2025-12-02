4136074216e4cd06fef6ef99306a4f040672f682
nallabellisatya64 <nallabellisatya64@gmail.com>
2025-12-02 16:06:07 +0530

    Add files via upload

---
+++ linear search of array.cpp
@@ -0,0 +1,25 @@
+#include<stdio.h>
+int main()
+{
+	int a[100],n,i,x,flag=0; 
+	printf("Enter size of the Array");
+	scanf("%d",&n);
+	printf("Enter array values");
+	for(i=0;i<n;i++)
+	{
+        scanf("%d",&a[i]);
+	}
+	printf("Enter search element");
+	scanf("%d",&x);
+	for(i=0;i<n;i++)
+	{
+		if(x==a[i])
+		{
+			printf("Element is found");
+			flag=1;
+		}
+	}
+	if(flag==0)
+	printf("Element is Not found");
+	return 0;
+}
